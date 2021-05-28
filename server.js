21eb3467b4c313cac526ef308bf528eef270f7d0
Levi Kane <oneleggedlevi09@gmail.com>
2021-05-27 19:53:24 -0500

    third heroku deployment try

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const compression = require('compression');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/workout';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/';
 
 mongoose.connect(MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
   useNewUrlParser: true,
